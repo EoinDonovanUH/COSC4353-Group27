5d9e524fe3cdf9291bc469f9e503e8e472982388
AlexFlores210 <112830731+AlexFlores210@users.noreply.github.com>
2023-03-24 20:05:01 -0500

    added priceing class in models folder

---
+++ models/PriceMode.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+class PricingModule{
+    //placeholder values for the pricing module to calculate a price to return.
+    constructor(distance,fuelPrice,gallonsRec)
+    {
+        /*
+        set varibales here for cost calcualtion
+        */
+    }
+    calcCost(){
+        //using user input calculate and return price to be charged
+    }
+}
