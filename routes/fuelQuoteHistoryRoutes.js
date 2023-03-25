69a10b8c620a818f7e4812ec87e6123fe906017b
JaredzG <82556918+JaredzG@users.noreply.github.com>
2023-03-24 20:40:02 -0500

    Update fuel quote form and history routes

--- routes/fuelQuoteHistoryRoutes.js
+++ routes/fuelQuoteHistoryRoutes.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
-const fuelQuoteHistoryController = require("../controllers/fuelQuoteFormController");
+// const path = require("path");
+const fuelQuoteHistoryController = require("../controllers/fuelQuoteHistoryController");
 
-router.get("^/$|/quotehistory(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "views", "quotehistory.html"));
-});
+// router.get("^/$|/quotehistory(.html)?", (req, res) => {
+//   res.sendFile(path.join(__dirname, "..", "views", "quotehistory.html"));
+// });
 
 router
-  .route("/")
+  .route("/:id")
   // HTTP method                    // CRUD operation
-  .get() // read
+  .get(fuelQuoteHistoryController.getFuelQuoteHistory) // read
   .post() // create
   .patch() // update
   .delete(); // delete
