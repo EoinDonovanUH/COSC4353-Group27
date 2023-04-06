5f2e35a534f8d59dca8ccd6a051b6da5a6cfc773
Noah Rhodes <rhodesnoahd@gmail.com>
2023-04-06 13:44:15 -0500

    added Alex's pricing module

---
+++ backend/modules/Pricing.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+class Pricing {
+    //placeholder values for the pricing module to calculate a price to return.
+    constructor(distance,fuelPrice,gallonsRec)
+    {
+        /*
+        set varibales here for cost calcualtion
+        use APIs to set variables for price calculation
+        */
+    }
+    calcCost(){
+        //using user input calculate and return price to be charged
+    }
+}
